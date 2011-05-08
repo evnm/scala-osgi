947451783cfb03b2caea01e57a9b0256feb7e960
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-05-08 15:49:17 -0700

    Package scala-osgi as an OSGi bundle

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,13 @@
 import sbt._
+import com.weiglewilczek.bnd4sbt.BNDPlugin
 
-class ScalaOSGiProject(info: ProjectInfo) extends DefaultProject(info) {
+class ScalaOSGiProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
   val osgiCore = "org.osgi" % "org.osgi.core" % "4.2.0" % "provided"
   val osgiCompendium = "org.osgi" % "org.osgi.compendium" % "4.2.0" % "provided"
+
+  override def bndBundleSymbolicName = "com.kiip.osgi"
+  override def bndBundleVendor = Some("Kiip")
+  override def bndBundleLicense =
+    Some("Apache 2.0 License (http://www.apache.org/licenses/LICENSE-2.0.html)")
+  override def bndExportPackage = "com.kiip.osgi;version=\"%s\"".format(projectVersion.value) :: Nil
 }
